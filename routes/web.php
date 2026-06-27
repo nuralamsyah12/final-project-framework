5b7256ca9eaf48e91c9713e1fca7bcaaff3794cc
nuralamsyah12 <116506370+nuralamsyah12@users.noreply.github.com>
2026-06-27 10:32:42 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 
+// 1. HALAMAN UTAMA (Langsung diarahkan ke form login)
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
+// 2. RUTE UNTUK PROSES AUTHENTICATION (Bawaan Laravel Breeze)
+require __DIR__.'/auth.php';
+
+// 3. RUTE YANG DIKUNCI (Hanya bisa diakses jika sudah login)
 Route::middleware(['auth'])->group(function () {
     
+    // Halaman Beranda Utama
     Route::get('/beranda', function () {
         $semuaBarang = \App\Models\Barang::all();
         $totalStok = \App\Models\Barang::sum('stok');
         return view('beranda', compact('semuaBarang', 'totalStok'));
     })->name('beranda');
 
+    // CRUD Barang
     Route::resource('barang', BarangController::class);
 });
